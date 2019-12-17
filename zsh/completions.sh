6ef30b4af0cc33b65da019ff882183918eb96c87
Ting Zhou <zhoutingneyc@gmail.com>
2019-12-17 01:10:10 +0000

    Fixed ctags for extensionless files
    Also added completion for blaze

--- zsh/completions.sh
+++ zsh/completions.sh
@@ -35,3 +35,25 @@ _fzf_complete_g4cd_post() {
   # remove the marker name bits
   cut -f2 -d$'\t'
 }
+
+_fzf_complete_bb() {
+  _fzf_complete '+m' "$@" < <(
+    # all files under current dir
+    cat BUILD | egrep '^\s+name \=' | sed "s/^[^\"]*\"//" | sed "s/\".*$//"
+  )
+  FZF_DEFAULT_OPTS="--height 40% --reverse"
+}
+
+_fzf_complete_bt() {
+  _fzf_complete '+m' "$@" < <(
+    # all files under current dir
+    cat BUILD | egrep '^\s+name \=' | sed "s/^[^\"]*\"//" | sed "s/\".*$//"
+  )
+}
+
+_fzf_complete_br() {
+  _fzf_complete '+m' "$@" < <(
+    # all files under current dir
+    cat BUILD | egrep '^\s+name \=' | sed "s/^[^\"]*\"//" | sed "s/\".*$//"
+  )
+}

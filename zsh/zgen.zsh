ce28c578a858f5171a37e950723e29bee0d699f7
Ting Zhou <zhoutingneyc@gmail.com>
2024-04-08 21:37:04 -0700

    removes dependency on oh-my-zsh

--- zsh/zgen.zsh
+++ zsh/zgen.zsh
@@ -1,19 +1,10 @@
 if ! zgen saved; then
-  zgen oh-my-zsh
-
   # plugins
+  # shell prompt creator
   zgen load romkatv/powerlevel10k powerlevel10k
   zgen load zsh-users/zsh-syntax-highlighting
   zgen load zsh-users/zsh-autosuggestions
-  zgen load jocelynmallon/zshmarks
   zgen load ael-code/zsh-colored-man-pages
-  # zgen load rupa/z
-  # zgen load changyuheng/fz
-  zgen oh-my-zsh plugins/git
-  zgen oh-my-zsh plugins/fd
-  if type "hg" > /dev/null; then
-    zgen oh-my-zsh plugins/mercurial
-  fi
   # save all to init script
   zgen save
 fi

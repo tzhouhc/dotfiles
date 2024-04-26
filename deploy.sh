b95c1623f2f0c5619d593c1dcd41250523d4b3c5
Ting Zhou <zhoutingneyc@gmail.com>
2024-04-26 02:08:44 -0700

    adds zellij

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,16 @@ set -e
 if ! [ -d "$HOME/.fzf" ]; then
   git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
   ~/.fzf/install
+  echo "Installed FZF"
+else
+  echo "FZF already present"
 fi
 # zgen (handles zsh)
 if ! [ -e "$HOME/.zgen" ]; then
   git clone https://github.com/tarjoilija/zgen.git $HOME/.zgen
+  echo "Installed Zgen"
+else
+  echo "Zgen already present"
 fi
 
 # current script location
@@ -33,7 +39,9 @@ ln -sf "$cwd/zshrc" "$HOME/.zshrc"
 ln -sf "$config_dir/batrc" "$HOME/.batrc"
 ln -sf "$config_dir/ripgreprc" "$HOME/.ripgreprc"
 ln -sf "$config_dir/pythonrc" "$HOME/.pythonrc"
-ln -sf "$config_dir/hammerspoon" "$HOME/.hammerspoon"
+echo "(Re)linked RC files"
+
+ln -sfT "$config_dir/hammerspoon" "$HOME/.hammerspoon"
 
 mkdir -p "$HOME/.data/zoxide"
 
@@ -45,8 +53,11 @@ ln -sfT "$cwd/zsh" "$HOME/.zsh"
 mkdir -p "$xdg_dir/git"
 ln -sf "$config_dir/gitconfig" "$xdg_dir/git/config"
 ln -sf "$config_dir/gitignore" "$xdg_dir/git/ignore"
-ln -sf "$config_dir/kitty" "$xdg_dir/kitty"
 ln -sf "$config_dir/gitignore" "$HOME/.gitignore"
+
+# new experimental tools
+ln -sfT "$config_dir/kitty" "$xdg_dir/kitty"
+ln -sfT "$config_dir/zellij" "$xdg_dir/zellij"
 
 # fd
 mkdir -p "$xdg_dir/fd"
@@ -58,8 +69,10 @@ ln -sf "$config_dir/tmux.conf" "$xdg_dir/tmux/tmux.conf"
 # tpm
 if ! [ -d "$xdg_dir/tmux/plugins/tpm" ]; then
   git clone https://github.com/tmux-plugins/tpm $xdg_dir/tmux/plugins/tpm
+  echo "Installing TPM"
+else
+  echo "TPM already installed"
 fi
-
 
 # ctags
 ln -sfT "$config_dir/ctags" "$xdg_dir/ctags"

52ce20c9ab05bc22ff249d8efd302e7d27044eef
Ting Zhou <zhoutingneyc@gmail.com>
2024-05-12 20:10:06 -0700

    minor tmux tweak

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -1,15 +1,9 @@
 if [[ "$NOTMUX" == 'true' ]]; then
-else
-  if [ -z "$TMUX" ]; then
-    if [[ -e ~/.notmux ]]; then
-      # skip tmux
-    else
-      if [[ "$IS_GOOGLE" == true ]]; then
-        # google's tmux variant that allows gnub auth
-        tmx2 attach
-      else
-        tmux attach
-      fi
-    fi
-  fi
+  return
 fi
+
+if [[ -e ~/.notmux ]]; then
+  return
+fi
+
+tmux attach

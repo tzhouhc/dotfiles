91b857390d4c82ec6c0760d954fb7ec80dcf4f4f
Ting Zhou <zhoutingneyc@gmail.com>
2024-06-11 05:10:37 +0000

    fix(prompt): adds fallback for nfv2 for the prompt

--- zsh/shell/tools.zsh
+++ zsh/shell/tools.zsh
@@ -7,7 +7,11 @@ fi
 
 # oh-my-posh prompt
 if type oh-my-posh >/dev/null 2>&1; then
-  eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/oh-my-posh/prompt.json)"
+  if [[ $NERDFONT == '2' ]]; then
+    eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/oh-my-posh/prompt_backup.json)"
+  else
+    eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/oh-my-posh/prompt.json)"
+  fi
 elif type starship &>/dev/null; then
   # starship prompt (no transient prompt)
   eval "$(starship init zsh)"

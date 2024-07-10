9e7283a699451b4891325ac1fe9eca3f34455f7f
Ting Zhou <zhoutingneyc@gmail.com>
2024-07-10 19:12:31 +0800

    feat(hs): allow adjusting window size to thirds/quarters

--- configs/dot-hammerspoon/windows.lua
+++ configs/dot-hammerspoon/windows.lua
@@ -5,19 +5,27 @@ hs.window.animationDuration = 0
 
 local M = {}
 
+-- alternates between right half/third/quarter.
 function M.right_half()
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x + (max.w / 2)
   f.y = max.y
-  f.w = max.w / 2
+  if f.w == max.w / 3 then
+    f.w = max.w / 4
+  elseif f.w == max.w / 2 then
+    f.w = max.w / 3
+  else
+    f.w = max.w / 2
+  end
+  f.x = max.x + max.w - f.w
   f.h = max.h
   win:setFrame(f)
 end
 
+-- alternates between left half/third/quarter.
 function M.left_half()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -26,7 +34,13 @@ function M.left_half()
 
   f.x = max.x
   f.y = max.y
-  f.w = max.w / 2
+  if f.w == max.w / 3 then
+    f.w = max.w / 4
+  elseif f.w == max.w / 2 then
+    f.w = max.w / 3
+  else
+    f.w = max.w / 2
+  end
   f.h = max.h
   win:setFrame(f)
 end
@@ -44,6 +58,8 @@ function M.maximize()
   win:setFrame(f)
 end
 
+-- alternates between bottom half/third/quarter.
+-- window might not actually allow the resizing.
 function M.bottom_half()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -51,12 +67,20 @@ function M.bottom_half()
   local max = screen:frame()
 
   f.x = max.x
-  f.y = max.y + (max.h / 2)
   f.w = max.w
-  f.h = max.h / 2
+  if f.h == max.h / 3 then
+    f.h = max.h / 4
+  elseif f.h == max.h / 2 then
+    f.h = max.h / 3
+  else
+    f.h = max.h / 2
+  end
+  f.y = max.y + max.h - f.h
   win:setFrame(f)
 end
 
+-- alternates between top half/third/quarter.
+-- window might not actually allow the resizing.
 function M.top_half()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -66,7 +90,13 @@ function M.top_half()
   f.x = max.x
   f.y = max.y / 2
   f.w = max.w
-  f.h = max.h / 2
+  if f.h == max.h / 3 then
+    f.h = max.h / 4
+  elseif f.h == max.h / 2 then
+    f.h = max.h / 3
+  else
+    f.h = max.h / 2
+  end
   win:setFrame(f)
 end
 
@@ -79,7 +109,13 @@ function M.top_left_quadrant()
 
   f.x = max.x
   f.y = max.y / 2
-  f.w = max.w / 2
+  if f.w == max.w / 3 then
+    f.w = max.w / 4
+  elseif f.w == max.w / 2 then
+    f.w = max.w / 3
+  else
+    f.w = max.w / 2
+  end
   f.h = max.h / 2
   win:setFrame(f)
 end
@@ -90,9 +126,15 @@ function M.top_right_quadrant()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x + (max.w / 2)
   f.y = max.y / 2
-  f.w = max.w / 2
+  if f.w == max.w / 3 then
+    f.w = max.w / 4
+  elseif f.w == max.w / 2 then
+    f.w = max.w / 3
+  else
+    f.w = max.w / 2
+  end
+  f.x = max.x + max.w - f.w
   f.h = max.h / 2
   win:setFrame(f)
 end
@@ -105,7 +147,13 @@ function M.bottom_left_quadrant()
 
   f.x = max.x
   f.y = max.y + (max.h / 2)
-  f.w = max.w / 2
+  if f.w == max.w / 3 then
+    f.w = max.w / 4
+  elseif f.w == max.w / 2 then
+    f.w = max.w / 3
+  else
+    f.w = max.w / 2
+  end
   f.h = max.h / 2
   win:setFrame(f)
 end
@@ -116,9 +164,15 @@ function M.bottom_right_quadrant()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x + (max.w / 2)
   f.y = max.y + (max.h / 2)
-  f.w = max.w / 2
+  if f.w == max.w / 3 then
+    f.w = max.w / 4
+  elseif f.w == max.w / 2 then
+    f.w = max.w / 3
+  else
+    f.w = max.w / 2
+  end
+  f.x = max.x + max.w - f.w
   f.h = max.h / 2
   win:setFrame(f)
 end

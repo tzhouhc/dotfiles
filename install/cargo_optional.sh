1a783c0a657a475b362cf62abeb14f84a05bb100
Ting Zhou <zhoutingneyc@gmail.com>
2024-12-19 11:27:08 -0500

    refactor: installation scripts

--- install/cargo_optional.sh
+++ install/cargo_optional.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 set -e
 
-cargo install --locked --git https://github.com/asciinema/asciinema  # recording
 cargo install --locked bacon  # rust compiling and testing
 cargo install --locked typst-cli  # latex replacement
 
-cargo install aichat  # chatgpt, gemini, etc
 cargo install atac  # requests
-cargo install bottom --locked  # top
-cargo install du-dust  # better disk usage display
 cargo install evcxr_repl  # repl for rust
-cargo install lolcate-rs  # locate indexer database
 cargo install nu  # shell for data
-cargo install pipr  # pipeline crafter
 cargo install procs  # ps
 cargo install tv  # csv viewer
 cargo install zellij  # tmux

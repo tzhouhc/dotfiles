9c1c2bb9d7e617999050fbc8eb30c83424594ba3
Ting Zhou <zhoutingneyc@gmail.com>
2025-02-03 22:55:35 -0500

    fix: silence wezterm warning about missing glyphs

--- xdg_configs/wezterm/settings/common.lua
+++ xdg_configs/wezterm/settings/common.lua
@@ -4,6 +4,7 @@ function M.update_config(config)
   -- quit when asked to
   config.exit_behavior = 'Close'
   config.window_close_confirmation = 'NeverPrompt'
+  config.warn_about_missing_glyphs = false
 end
 
 return M

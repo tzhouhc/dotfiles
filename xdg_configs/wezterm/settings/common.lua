fe86a88fff95c945d38436d329b7b3351adb156e
Ting Zhou <zhoutingneyc@gmail.com>
2025-02-10 14:59:54 -0500

    fix: omp display; wezterm tab indices

--- xdg_configs/wezterm/settings/common.lua
+++ xdg_configs/wezterm/settings/common.lua
@@ -5,6 +5,7 @@ function M.update_config(config)
   config.exit_behavior = 'Close'
   config.window_close_confirmation = 'NeverPrompt'
   config.warn_about_missing_glyphs = false
+  config.tab_and_split_indices_are_zero_based = false
 end
 
 return M

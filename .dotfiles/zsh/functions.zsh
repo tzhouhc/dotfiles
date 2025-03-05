91dce76d9a4080e800cd295599fece4fae93fea7
Ting Zhou <zhoutingneyc@gmail.com>
2025-03-05 16:07:29 -0500

    refactor: remove zsh scripts to prevent ambiguity

--- .dotfiles/zsh/functions.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-# Overall collection of various shell functions available for use
-#
-# Can be sourced as a standalone file for effect.
-
-source $HOME/.zsh/lib/utils.zsh
-source $HOME/.zsh/lib/shorthands.zsh
-source $HOME/.zsh/lib/fzf.zsh
--- .dotfiles/zsh/shell/post_tmux.zsh
+++
@@ -1,2 +0,0 @@
-# Setting FZF defaults before TMUX will mess with ftb-tmux-popup's sizing
-export FZF_DEFAULT_OPTS="--height 40% --reverse"

13eb8e39dc551841b4d1e10ce9a2b46cb0ca9bdc
Ting Zhou <zhoutingneyc@gmail.com>
2025-05-07 17:14:23 -0400

    refactor: use requirements.txt to specify python reqs

--- .dotfiles/install/pip.sh
+++ .dotfiles/install/pip.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+cd "$(dirname "$0")"
+
 if ! type "pip3" > /dev/null; then
   echo "pip3 not found!"
   exit 1
@@ -9,4 +11,4 @@ fi
 # neovim compat and nvr for client-server mode
 # using --break-system-packages flag; should _not_ be using system pkg manager
 # for python packages anyway.
-pip3 install neovim neovim-remote --break-system-packages
+python3 -m pip install -r ./py_requirements.txt --break-system-packages

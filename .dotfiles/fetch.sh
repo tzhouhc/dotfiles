4555d75d432a675d011ae0859a0dd5a2ac04d9ee
Ting Zhou <zhoutingneyc@gmail.com>
2025-10-04 18:51:58 +0000

    fix: fetch.sh

--- .dotfiles/fetch.sh
+++ .dotfiles/fetch.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
+dfg() {
+  git --git-dir="$HOME/.dotfiles.git" --work-tree="$HOME" $@
+}
+
 git clone --bare git@github.com:tzhouhc/dotfiles.git $HOME/.dotfiles.git
-alias dfg='git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME'
+
 dfg checkout -f
 dfg submodule update --init --recursive
+
 cp $HOME/.dotfiles/ref_git_config $HOME/.dotfiles.git/config
 
 source $HOME/.dotfiles/install.sh

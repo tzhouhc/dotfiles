d1190afbe67ed55c89f375adb7e859001cc5fe14
Ting Zhou <zhoutingneyc@gmail.com>
2025-10-10 10:56:23 -0400

    feat: raycast scripts dir and sample script

---
+++ .config/raycast/scripts/w4118.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open W4118 org page
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName GitHub
+# @raycast.icon images/github-logo.png
+# @raycast.iconDark images/github-logo-iconDark.png
+
+open https://github.com/w4118

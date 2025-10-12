6d792e4d47060ebbbff5025954f01b7bdb78a754
Ting Zhou <zhoutingneyc@gmail.com>
2025-10-12 01:53:47 +0000

    feat: gh auth for the credentials scripts

--- .dotfiles/install/creds.sh
+++ .dotfiles/install/creds.sh
@@ -4,22 +4,48 @@ source $HOME/.zsh/base.zsh
 
 mkdir -p ~/.credentials
 
-openai_clear=$(fetch-secret bece5e4b282e2832880f2a87bf4329f1)
-deepseek_clear=$(877f672099afd3e354d63cadea5c14b1)
-github_clear=$(fetch-secret 9b9e18cebd82a343bb12d4911bc1653c)
+if ! gh auth status >/dev/null 2>&1; then
+  echo "Logging into Github."
+  # always use default github.com;
+  # always use ssh key for auth;
+  # always use web interface, which will fail, then perform the login manually
+  # from the host.
+  gh auth login -p ssh -h github.com -w
+fi
+
+if [[ -f "$HOME/.credentials/openai_key" ]]; then
+  if gum confirm "Overwrite existing Openai key?"; then
+    openai_clear=$(fetch-secret bece5e4b282e2832880f2a87bf4329f1)
+  fi
+fi
+
+if [[ -f "$HOME/.credentials/deepseek_key" ]]; then
+  if gum confirm "Overwrite existing Deepseek key?"; then
+    deepseek_clear=$(fetch-secret 877f672099afd3e354d63cadea5c14b1)
+  fi
+fi
+
+if [[ -f "$HOME/.credentials/github_key" ]]; then
+  if gum confirm "Overwrite existing Github token?"; then
+    github_clear=$(fetch-secret 9b9e18cebd82a343bb12d4911bc1653c)
+  fi
+fi
 
 if [[ ! -f "$HOME/.ssh/id_ed25519" ]]; then
   ssh-keygen
 fi
 
 if [[ -n "${openai_clear}" ]]; then
-  echo $openai_clear | age -R $HOME/.ssh/id_ed25519.pub -o $HOME/.credentials/openai_key
+  echo $openai_clear | age -R $HOME/.ssh/id_ed25519.pub \
+    -o $HOME/.credentials/openai_key
 fi
 
 if [[ -n "${deepseek_clear}" ]]; then
-  echo $deepseek_clear | age -R $HOME/.ssh/id_ed25519.pub -o $HOME/.credentials/deepseek_key
+  echo $deepseek_clear | age -R $HOME/.ssh/id_ed25519.pub \
+    -o $HOME/.credentials/deepseek_key
 fi
 
 if [[ -n "${github_clear}" ]]; then
-  echo $github_clear | age -R $HOME/.ssh/id_ed25519.pub -o $HOME/.credentials/github_key
+  echo $github_clear | age -R $HOME/.ssh/id_ed25519.pub -o \
+    $HOME/.credentials/github_key
 fi

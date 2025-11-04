89d67266b0b512ae81b7c9adafcd119c313eb83b
Ting Zhou <zhoutingneyc@gmail.com>
2025-11-04 12:42:28 -0500

    cleanup: remove hoard
    It was a quick test; basically the gist is that I only really want an
    interactive interface to add commands quickly.

--- .dotfiles/install/cargo.sh
+++ .dotfiles/install/cargo.sh
@@ -25,11 +25,10 @@ if type cargo >/dev/null 2>&1; then
   cargo binstall -y erdtree   # tree
   cargo binstall -y fd-find   # find
   cargo binstall -y git-delta   # diff
-  cargo binstall -y hoard-rs   # hoard commands
   cargo binstall -y imgcatr  # show images
   cargo binstall -y jnv   # interactive jq
   cargo binstall -y just   # make
-  cargo binstall -y navi   # hoard commands too -- ?
+  cargo binstall -y navi   # save commands for later reuse
   cargo binstall -y petname  # random name generator
   cargo binstall -y zellij  # tmux
   cargo install ripgrep --features 'pcre2'  # grep
